a5900335d2295b32327f25905c6b9e1336cedcc4
Anthony Foiani <anthony.foiani@firstup.io>
2022-12-06 02:32:19 -0800

    README, first implementation, gitignore

---
+++ README.md
@@ -0,0 +1,75 @@
+# Man Pages for AWS CLI
+
+Many thanks to:
+
+https://tanguy.ortolo.eu/blog/article153/awscli-manpages
+
+Their solution is a bit slow, and it requires modifying installed files.
+
+My version uses monkey-patching to accomplish something similar.
+
+On a 2022 M2 laptop for v2.9.1, this version takes about 3.5 minutes to create about 12k files.
+
+You can regenerate them with `bin/gen-man-pages.py`.
+
+## Instructions
+
+Find the version of AWS CLI you're running:
+
+```console
+$ aws --version
+aws-cli/2.9.1 Python/3.11.0 Darwin/22.1.0 source/arm64 prompt/off
+```
+
+Clone this repo and take note of where it ended up:
+
+```console
+$ cd
+
+$ mkdir -p Source
+
+$ cd Source
+
+$ git clone https://github.com/ajf-firstup/homebrew-awscli-manpages
+
+$ cd homebrew-awscli-manpages
+
+$ AWSCLI_MANPAGES_DIR="$PWD"
+```
+
+Find the matching man page sources and symlink them into `/opt/homebrew/share/man/man1aws`:
+
+```console
+$ cd /opt/homebrew/share/man/man1aws
+
+$ ln -s "$AWSCLI_MANPAGES_DIR/v2.9.1" man1aws
+```
+
+Then modify your `MANSECT` environment variable (or use the `-S` argument to `man`) to include the
+`1aws` section:
+
+```console
+$ man -S 1aws aws-elbv2
+ELBV2(1aws)                         awscli                         ELBV2(1aws)
+
+NAME
+       elbv2 -
+
+DESCRIPTION
+       A load balancer distributes incoming traffic across targets, such as
+       your EC2 instances. This enables you to increase the availability of
+       your application. The load balancer also monitors the health of ...
+
+...
+
+2.9.1                                                              ELBV2(1aws)
+
+```
+
+TODO(ajf): Maybe figure out how to invoke `makewhatis` to re-index the new files?
+
+TODO(ajf): Maybe write a wrapper to automatically detect the AWS CLI version and do all the magic
+from that point forward?
+
+TODO(ajf): How to ensure that our monkey patching is suitable? Possibly allow-list known-compatible
+AWS CLI versions?
---
+++ bin/gen-man-pages.py
@@ -0,0 +1,59 @@
+#!/opt/homebrew/Cellar/awscli/2.9.1/libexec/bin/python3.11
+#
+# More info / inspiration:
+#
+#   https://tanguy.ortolo.eu/blog/article153/awscli-manpages
+#
+
+import subprocess
+import awscli.clidriver
+import awscli.help
+import gzip
+
+# --------------------------------------------------------------------------------------------------
+# The libraries don't expose customization points for these, so we make our own!
+
+from docutils.core import publish_string
+from docutils.writers import manpage
+
+class TroffHelpRenderer(object):
+    def render(self, contents):
+        writer = manpage.Writer()
+        global output_file
+        output_file.write(publish_string(contents, writer=writer))
+
+awscli.help.get_renderer = lambda: TroffHelpRenderer()
+
+def manpage_translate_with_docinfo(manpage_writer):
+    visitor = manpage_writer.translator_class(manpage_writer.document)
+    visitor._docinfo['manual_group']   = 'awscli'
+    visitor._docinfo['manual_section'] = '1aws'
+    visitor._docinfo['version']        = '2.9.1'
+    manpage_writer.document.walkabout(visitor)
+    manpage_writer.output = visitor.astext()
+
+manpage.Writer.translate = manpage_translate_with_docinfo
+
+# --------------------------------------------------------------------------------------------------
+
+driver = awscli.clidriver.CLIDriver()
+
+def write_manpage(command):
+    filename = '%s.1aws.gz' % '-'.join(command)
+    command = command[1:]  # Strip off leading `aws`.
+    command.append('help')
+    print(filename)
+    global output_file
+    # output_file = open(filename, 'wb')
+    output_file = gzip.open(filename, mode='wb', compresslevel=9)
+    global driver
+    driver.main(command)
+
+# --------------------------------------------------------------------------------------------------
+
+write_manpage(['aws'])
+for command, subcommands in driver._get_command_table().items():
+    write_manpage(['aws', command])
+    if command != 'help':
+        for subcommand in subcommands._get_command_table():
+            write_manpage(['aws', command, subcommand])
